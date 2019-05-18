251c58dc72d87c960ea77f18d3e396603cb64aaa
Zvi Baratz <z.baratz@gmail.com>
2019-05-18 12:27:20 +0300

    WIP: Adding DRF support

---
+++ django_mri/filters/__init__.py
@@ -0,0 +1,1 @@
+from django_mri.filters.scan_filter import ScanFilter
--- django_mri/views.py
+++ django_mri/views.py
@@ -2,8 +2,69 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView
 from django.views.generic.edit import ModelFormMixin
 from django_dicom.models import Series
+from django_mri.filters.scan_filter import ScanFilter
 from django_mri.forms import ScanReview
 from django_mri.models import Scan
+from django_mri.serializers import ScanSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import authentication, filters, permissions, viewsets
+
+
+class DefaultsMixin:
+    """
+    Default settings for view authentication, permissions, filtering and pagination.
+    
+    """
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (permissions.IsAuthenticated,)
+    paginate_by = 25
+    paginate_by_param = "page_size"
+    max_paginate_by = 100
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
+
+class ScanViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """
+    API endpoint that allows scans to be viewed or edited.
+    """
+
+    queryset = Scan.objects.all().order_by("-time")
+    serializer_class = ScanSerializer
+    filter_class = ScanFilter
+    search_fields = (
+        "id",
+        "description",
+        "number",
+        "created",
+        "scan_time",
+        "echo_time",
+        "inversion_time",
+        "repetition_time",
+        "sequence_type",
+        "spatial_resolution",
+        "institution_name",
+        "is_updated_from_dicom",
+    )
+    ordering_fields = (
+        "id",
+        "description",
+        "number",
+        "created",
+        "echo_time",
+        "inversion_time",
+        "repetition_time",
+        "sequence_type",
+        "spatial_resolution",
+        "institution_name",
+    )
 
 
 class CreateScanView(CreateView, ModelFormMixin, LoginRequiredMixin):
