e060984016db0fdc8fdbfaf4a25c6f02c48d9c66
Zvi Baratz <z.baratz@gmail.com>
2019-06-05 17:22:08 +0300

    Added common sequences description

--- django_mri/models/common_sequences.py
+++ django_mri/models/common_sequences.py
@@ -7,37 +7,37 @@ A list of common MRI sequences and their respective DICOM header attributes.
 sequences = [
     {
         "title": "MPRAGE",
-        "description": "",
+        "description": "A T1-weighted 3D magnetization prepared-rapid acquisition gradient echo sequence. See https://pubs.rsna.org/doi/10.1148/radiology.182.3.1535892 for more information.",
         "scanning_sequence": ["GR", "IR"],
         "sequence_variant": ["SK", "SP", "MP"],
     },
     {
         "title": "Localizer",
-        "description": "",
+        "description": "A set of three-plane, low-resolution, large field-of view scans that are obtained to be used for configuring the execution of subsequent scans.",
         "scanning_sequence": ["GR"],
         "sequence_variant": ["SP", "OSP"],
     },
     {
         "title": "IR-EPI",
-        "description": "",
+        "description": "180° inversion pulse to prepare magnetization and then a RF excitation pulse which results in T1-weighting.",
         "scanning_sequence": ["EP", "IR"],
         "sequence_variant": ["SK", "SP", "MP", "OSP"],
     },
     {
         "title": "FLAIR",
-        "description": "",
+        "description": "Fluid-attenuated inversion recovery is an MRI sequence with an inversion recovery set to null fluids. ",
         "scanning_sequence": ["SE", "IR"],
         "sequence_variant": ["SK", "SP", "MP"],
     },
     {
         "title": "DWI",
-        "description": "",
+        "description": "Diffusion-weighted magnetic resonance imaging (DWI or DW-MRI) is the use of specific MRI sequences as well as software that generates images from the resulting data, that uses the diffusion of water molecules to generate contrast in MR images.",
         "scanning_sequence": ["EP"],
         "sequence_variant": ["SK", "SP"],
     },
     {
         "title": "Resting-state fMRI",
-        "description": "",
+        "description": "Resting state fMRI (rsfMRI or R-fMRI) is a method of functional magnetic resonance imaging (fMRI) that is used in brain mapping to evaluate regional interactions that occur in a resting or task-negative state, when an explicit task is not being performed.",
         "scanning_sequence": ["EP"],
         "sequence_variant": ["SK", "SS"],
     },
