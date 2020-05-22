6be2572638a7bfe077fe0e8c48e59f46037d15d7
Zvi Baratz <z.baratz@gmail.com>
2020-05-22 23:15:54 +0300

    Fixed dependency definition to specify django_analyses and django_mri as 'dependency_links'.

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 import os
 from setuptools import find_packages, setup
 
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 with open("requirements.txt") as fh:
     requirements = fh.read().splitlines()
+    install_requires = [
+        requirement
+        for requirement in requirements
+        if not requirement.startswith("git+")
+    ]
+    dependency_links = [
+        requirement for requirement in requirements if requirement.startswith("git+")
+    ]
 
 with open("requirements-dev.txt") as fh:
     dev_requirements = fh.read().splitlines()
 
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django_mri",
@@ -28,7 +36,8 @@ setup(
     author_email="baratzz@pm.me",
     keywords="django mri neuroimaging research",
     python_requires=">=3.6",
-    install_requires=requirements,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     extras_require={"dev": dev_requirements},
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
