24d88d0037551abb5d480dfcfd85795c3cf9bef9
Noam Aharony <aharonyn@gmail.com>
2020-06-07 16:43:25 +0300

    Tests are working after changes made to migrations file. bval and bvec are problematic.

--- django_mri/utils/utils.py
+++ django_mri/utils/utils.py
@@ -7,13 +7,19 @@ DEFAULT_MRI_DIR_NAME = "MRI"
 
 
 def get_subject_model():
-    app_label, model_name = settings.SUBJECT_MODEL.split(".")
-    return apps.get_model(app_label=app_label, model_name=model_name)
+    # app_label, model_name = settings.SUBJECT_MODEL.split(".")
+    # return apps.get_model(
+    #     app_label=app_label, model_name=model_name, require_ready=False
+    # )
+    return apps.get_model(settings.SUBJECT_MODEL, require_ready=False)
 
 
 def get_group_model():
-    app_label, model_name = settings.STUDY_GROUP_MODEL.split(".")
-    return apps.get_model(app_label=app_label, model_name=model_name)
+    # app_label, model_name = settings.STUDY_GROUP_MODEL.split(".")
+    # return apps.get_model(
+    #     app_label=app_label, model_name=model_name, require_ready=False
+    # )
+    return apps.get_model(settings.STUDY_GROUP_MODEL, require_ready=False)
 
 
 def get_mri_root() -> Path:
--- tests/models_tests/test_nifti.py
+++ tests/models_tests/test_nifti.py
@@ -1,119 +1,119 @@
-# import os
-# import factory
-# import numpy as np
+import os
+import factory
+import numpy as np
 
-# from django.test import TestCase
-# from django_mri.models.nifti import NIfTI
-# from django_mri.models.scan import Scan
-# from django_dicom.models import Image
-# from tests.fixtures import (
-#     DICOM_SERIES_PATH,
-#     SIEMENS_DWI_SERIES,
-#     SIEMENS_DWI_SERIES_PATH,
-#     # IMPORTED_DICOM_FILES_PATH,
-#     # IMPORTED_DICOM_DWI_PATH,
-# )
-# from tests.models import Subject
+from django.test import TestCase
+from django_mri.models.nifti import NIfTI
+from django_mri.models.scan import Scan
+from django_dicom.models import Image
+from tests.fixtures import (
+    DICOM_SERIES_PATH,
+    SIEMENS_DWI_SERIES,
+    SIEMENS_DWI_SERIES_PATH,
+    # IMPORTED_DICOM_FILES_PATH,
+    # IMPORTED_DICOM_DWI_PATH,
+)
+from tests.models import Subject
 
-# # from tests.utils import restore_path
-# from django.db.models import signals
-# from pathlib import Path
+# from tests.utils import restore_path
+from django.db.models import signals
+from pathlib import Path
 
 
-# class NIfTIModelTestCase(TestCase):
-#     @classmethod
-#     @factory.django.mute_signals(signals.post_save)
-#     def setUpTestData(cls):
-#         cls.subject = Subject.objects.create()
-#         Image.objects.import_path(Path(DICOM_SERIES_PATH))
-#         Scan.objects.get_or_create(dicom=Image.objects.first().series)
-#         Image.objects.import_path(Path(SIEMENS_DWI_SERIES_PATH))
-#         Scan.objects.get_or_create(dicom=Image.objects.last().series)
+class NIfTIModelTestCase(TestCase):
+    @classmethod
+    @factory.django.mute_signals(signals.post_save)
+    def setUpTestData(cls):
+        cls.subject = Subject.objects.create()
+        Image.objects.import_path(Path(DICOM_SERIES_PATH))
+        Scan.objects.get_or_create(dicom=Image.objects.first().series)
+        Image.objects.import_path(Path(SIEMENS_DWI_SERIES_PATH))
+        Scan.objects.get_or_create(dicom=Image.objects.last().series)
 
-#     def setUp(self):
-#         self.simple_scan = Scan.objects.first()
-#         self.dwi_scan = Scan.objects.last()
-#         if not self.simple_scan or not self.dwi_scan:
-#             self.fail("Test scan not created! Check signals.")
-#         self.simple_nifti = self.simple_scan.dicom_to_nifti()
-#         self.dwi_nifti = self.dwi_scan.dicom_to_nifti()
+    def setUp(self):
+        self.simple_scan = Scan.objects.first()
+        self.dwi_scan = Scan.objects.last()
+        if not self.simple_scan or not self.dwi_scan:
+            self.fail("Test scan not created! Check signals.")
+        self.simple_nifti = self.simple_scan.dicom_to_nifti()
+        self.dwi_nifti = self.dwi_scan.dicom_to_nifti()
 
-#     # @classmethod
-#     # def tearDownClass(cls):
-#     #     scan = Scan.objects.last()
-#     #     dwi_scan = Scan.objects.first()
-#     #     curr_path = os.path.join(
-#     #         IMPORTED_DICOM_FILES_PATH, scan.dicom.patient.uid, scan.dicom.uid
-#     #     )
-#     #     dwi_curr_path = os.path.join(
-#     #         IMPORTED_DICOM_DWI_PATH, dwi_scan.dicom.patient.uid, dwi_scan.dicom.uid
-#     #     )
-#     #     restore_path(curr_path, DICOM_SERIES_PATH)
-#     #     restore_path(dwi_curr_path, SIEMENS_DWI_SERIES_PATH)
-#     #     super().tearDownClass()
+    # @classmethod
+    # def tearDownClass(cls):
+    #     scan = Scan.objects.last()
+    #     dwi_scan = Scan.objects.first()
+    #     curr_path = os.path.join(
+    #         IMPORTED_DICOM_FILES_PATH, scan.dicom.patient.uid, scan.dicom.uid
+    #     )
+    #     dwi_curr_path = os.path.join(
+    #         IMPORTED_DICOM_DWI_PATH, dwi_scan.dicom.patient.uid, dwi_scan.dicom.uid
+    #     )
+    #     restore_path(curr_path, DICOM_SERIES_PATH)
+    #     restore_path(dwi_curr_path, SIEMENS_DWI_SERIES_PATH)
+    #     super().tearDownClass()
 
-#     ##########
-#     # Fields #
-#     ##########
+    ##########
+    # Fields #
+    ##########
 
-#     # path
-#     def test_path_blank_and_null(self):
-#         field = NIfTI._meta.get_field("path")
-#         self.assertFalse(field.blank)
-#         self.assertFalse(field.null)
+    # path
+    def test_path_blank_and_null(self):
+        field = NIfTI._meta.get_field("path")
+        self.assertFalse(field.blank)
+        self.assertFalse(field.null)
 
-#     def test_path_max_length(self):
-#         field = NIfTI._meta.get_field("path")
-#         self.assertEqual(field.max_length, 1000)
+    def test_path_max_length(self):
+        field = NIfTI._meta.get_field("path")
+        self.assertEqual(field.max_length, 1000)
 
-#     def test_path_value(self):
-#         destination = self.dwi_scan.get_default_nifti_destination()
-#         expected = str(destination) + ".nii.gz"
-#         result = str(self.dwi_nifti.path)
-#         self.assertEqual(result, expected)
+    def test_path_value(self):
+        destination = self.dwi_scan.get_default_nifti_destination()
+        expected = str(destination) + ".nii.gz"
+        result = str(self.dwi_nifti.path)
+        self.assertEqual(result, expected)
 
-#     # is_raw
-#     def test_is_raw_blank_and_null(self):
-#         field = NIfTI._meta.get_field("is_raw")
-#         self.assertFalse(field.blank)
-#         self.assertFalse(field.null)
+    # is_raw
+    def test_is_raw_blank_and_null(self):
+        field = NIfTI._meta.get_field("is_raw")
+        self.assertFalse(field.blank)
+        self.assertFalse(field.null)
 
-#     def test_is_raw_default(self):
-#         field = NIfTI._meta.get_field("is_raw")
-#         self.assertFalse(field.default)
+    def test_is_raw_default(self):
+        field = NIfTI._meta.get_field("is_raw")
+        self.assertFalse(field.default)
 
-#     ###########
-#     # Methods #
-#     ###########
+    ###########
+    # Methods #
+    ###########
 
-#     def test_get_data(self):
-#         data = self.dwi_nifti.get_data()
-#         self.assertIsInstance(data, np.ndarray)
+    def test_get_data(self):
+        data = self.dwi_nifti.get_data()
+        self.assertIsInstance(data, np.ndarray)
 
-#     def test_get_b_value(self):
-#         result = self.dwi_nifti.get_b_value()
-#         self.assertListEqual(result, SIEMENS_DWI_SERIES["b_value"])
+    def test_get_b_value(self):
+        result = self.dwi_nifti.get_b_value()
+        self.assertListEqual(result, SIEMENS_DWI_SERIES["b_value"])
 
-#     def test_get_b_vector(self):
-#         result = self.dwi_nifti.get_b_vector()
-#         self.assertListEqual(result, SIEMENS_DWI_SERIES["b_vector"])
+    def test_get_b_vector(self):
+        result = self.dwi_nifti.get_b_vector()
+        self.assertListEqual(result, SIEMENS_DWI_SERIES["b_vector"])
 
-#     ##############
-#     # Properties #
-#     ##############
+    ##############
+    # Properties #
+    ##############
 
-#     def test_b_value(self):
-#         result = self.dwi_nifti.b_value
-#         expected = self.dwi_nifti.get_b_value()
-#         self.assertEqual(result, expected)
+    def test_b_value(self):
+        result = self.dwi_nifti.b_value
+        expected = self.dwi_nifti.get_b_value()
+        self.assertEqual(result, expected)
 
-#     def test_b_value_for_non_DWI_returns_none(self):
-#         self.assertIsNone(self.simple_nifti.b_value)
+    def test_b_value_for_non_DWI_returns_none(self):
+        self.assertIsNone(self.simple_nifti.b_value)
 
-#     def test_b_vector(self):
-#         result = self.dwi_nifti.b_vector
-#         expected = self.dwi_nifti.get_b_vector()
-#         self.assertListEqual(result, expected)
+    def test_b_vector(self):
+        result = self.dwi_nifti.b_vector
+        expected = self.dwi_nifti.get_b_vector()
+        self.assertListEqual(result, expected)
 
-#     def test_b_vector_for_non_DWI_returns_none(self):
-#         self.assertIsNone(self.simple_nifti.b_vector)
+    def test_b_vector_for_non_DWI_returns_none(self):
+        self.assertIsNone(self.simple_nifti.b_vector)
