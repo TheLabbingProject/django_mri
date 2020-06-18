ea4c3caa6c7f0e61020c08600ca5395c646cae83
Noam Aharony <aharonyn@gmail.com>
2020-06-18 09:16:37 +0300

    Added tests for analysis_definitions and pipeline_definitions. Commented out the recon_all method in Scan model.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -6,8 +6,9 @@ from django_analyses.models import (
     Analysis,
     InputSpecification,
     OutputSpecification,
+    Pipeline,
 )
-from django_mri.models import Scan
+from django_mri.models import Scan, NIfTI
 from django_mri.analysis.analysis_definitions import analysis_definitions
 from tests.models import Subject
 from tests.fixtures import (
@@ -26,12 +27,21 @@ class AnalysisModelTestCase(TestCase):
     def setUpTestData(cls):
         subject = Subject.objects.create()
         Image.objects.import_path(Path(SIEMENS_DWI_SERIES_PATH))
-        # analyses = Analysis.objects.from_list(analysis_definitions)
         scan = Scan.objects.create(dicom=Image.objects.first().series, subject=subject)
 
     def setUp(self):
         self.scan = Scan.objects.last()
+        self.analyses = Analysis.objects.from_list(analysis_definitions)
 
-    # def test_recon_all(self):
-    # result = self.scan.recon_all()
-    # self.assertIsNotNone(result)
+    def test_analyses_creation(self):
+        self.assertIsNotNone(self.analyses)
+        self.assertIsInstance(self.analyses, dict)
+
+    def test_pipline_creation(self):
+        nifti_path = Path("MNI152_T1_2mm_brain/1.nii.gz")
+        NIfTI.objects.create(path=nifti_path)
+        from django_mri.analysis.pipeline_definitions import pipeline_definitions
+
+        pipelines = Pipeline.objects.from_list(pipeline_definitions)
+        self.assertIsNotNone(pipelines)
+        self.assertIsInstance(pipelines, list)
