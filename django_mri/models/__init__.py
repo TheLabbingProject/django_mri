00945058eef8e318a02c1c419ce94dba0a1ee3cb
Noam Aharony <aharonyn@gmail.com>
2020-07-14 13:44:09 +0300

    Created SequenceTypeDefinition model and made a SequenceType Foreign Key in it. Made necessary changes to migrations.

--- django_mri/models/__init__.py
+++ django_mri/models/__init__.py
@@ -1,4 +1,5 @@
 from django_mri.models.scan import Scan
 from django_mri.models.nifti import NIfTI
+from django_mri.models.sequence_type_definition import SequenceTypeDefinition
 from django_mri.models.inputs import *
 from django_mri.models.outputs import *
--- django_mri/models/sequence_type.py
+++ django_mri/models/sequence_type.py
@@ -1,33 +1,17 @@
-from django.db import models
-from django_extensions.db.models import TitleDescriptionModel
-from django_mri.models.choices import ScanningSequence, SequenceVariant
-from django_mri.models.fields import ChoiceArrayField
+from django_extensions.db.models import TitleDescriptionModel, TimeStampedModel
 
 
-class SequenceType(TitleDescriptionModel):
+class SequenceType(TitleDescriptionModel, TimeStampedModel):
     """
-    A model to represent familiar MRI sequences. The purpose of this model is to
-    provide a title and description for commonly used sequences, as well as to
-    facilitate queries.
+    The purpose of this model is to provide a title and description for
+     commonly used sequences, as well as to facilitate queries.
     Each particular sequence is defined as a unique combination of DICOM's
     "*ScanningSequence*" and "*SequenceVariant*" attributes.
+    This model is a wrapper for the known combinations.
 
     """
 
-    scanning_sequence = ChoiceArrayField(
-        models.CharField(max_length=2, choices=ScanningSequence.choices()),
-        size=5,
-        blank=True,
-        null=True,
-    )
-    sequence_variant = ChoiceArrayField(
-        models.CharField(max_length=4, choices=SequenceVariant.choices()),
-        blank=True,
-        null=True,
-    )
-
     class Meta:
-        unique_together = ("scanning_sequence", "sequence_variant")
         ordering = ("title",)
 
     def __str__(self) -> str:
