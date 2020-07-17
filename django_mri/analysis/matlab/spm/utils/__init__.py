8fdda2fca63f5d80228a9efafca20a3bd414a4c7
Zvi Baratz <z.baratz@gmail.com>
2020-07-17 22:44:11 +0300

    Added docstrings to the SPM utils module that I missed.

--- django_mri/analysis/matlab/spm/utils/__init__.py
+++ django_mri/analysis/matlab/spm/utils/__init__.py
@@ -1,1 +1,5 @@
+"""
+Utilities for the :mod:`~django_mri.analysis.matlab.spm` module.
+"""
+
 from django_mri.analysis.matlab.spm.utils.nifti_validator import NiftiValidator
--- django_mri/analysis/matlab/spm/utils/batch_templates.py
+++ django_mri/analysis/matlab/spm/utils/batch_templates.py
@@ -1,5 +1,11 @@
+"""
+Definition of the :attr:`~django_mri.analysis.matlab.spm.utils.TEMPLATES`
+constant.
+"""
+
 from django_mri.analysis.matlab.spm.cat12.utils.batch_templates import (
     CAT12_TEMPLATES,
 )
 
+#: Batch templates dictionary, associating interface keys with template paths.
 TEMPLATES = CAT12_TEMPLATES
