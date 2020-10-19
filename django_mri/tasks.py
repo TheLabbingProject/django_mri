e604b264bd6f554c6aa627eb87ef9145d0cd03bb
Zvi Baratz <z.baratz@gmail.com>
2020-10-19 15:40:38 +0300

    Changed the import_data task's return value to a list and created a docstring.

--- django_mri/tasks.py
+++ django_mri/tasks.py
@@ -3,7 +3,23 @@ from django_mri.models.data_directory import DataDirectory
 
 
 @shared_task(name="django_mri.import-data")
-def import_data(data_directory_id: int) -> None:
+def import_data(data_directory_id: int) -> list:
+    """
+    Imports new data (unfamiliar subdirectories) from the provided
+    :class:`~django_mri.models.data_directory.DataDirectory` instance.
+
+    Parameters
+    ----------
+    data_directory_id : int
+        DataDirectory instance ID
+
+    Returns
+    -------
+    list
+        The names of the subdirectories within the DataDirectory that were
+        imported
+    """
+
     source = DataDirectory.objects.get(id=data_directory_id)
     imported_subdirectories = source.sync(progressbar=False, report=True)
-    return {"imported": imported_subdirectories}
+    return imported_subdirectories
