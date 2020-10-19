59c9d200ca2b3c3c8d00f83b47fe210b733d2327
Zvi Baratz <z.baratz@gmail.com>
2020-10-19 16:10:56 +0300

    Changed ScanManager to ScanQuerySet so that methods will be available from querysets and changed the 'scans_by_method()' method to 'filter_by_sequence_type()' and refactored it a bit. Also added some docstrings.

--- django_mri/models/managers/scan.py
+++ django_mri/models/managers/scan.py
@@ -1,14 +1,44 @@
-from django.db.models import Manager
+"""
+Definition of the :class:`ScanQuerySet` class.
+"""
+
+from django.db.models import Q, QuerySet
 from django_dicom.models.image import Image as DicomImage
 from django_mri.utils.scan_type import ScanType
 from django_mri.models.sequence_type import SequenceType
 from pathlib import Path
 
 
-class ScanManager(Manager):
+class ScanQuerySet(QuerySet):
+    """
+    Custom manager for the :class:`~django_mri.models.scan.Scan` class.
+    """
+
     def import_dicom_data(
         self, path: Path, progressbar: bool = True, report: bool = True
-    ) -> tuple:
+    ) -> QuerySet:
+        """
+        Imports DICOM files to the database using django_dicom_.
+
+        .. _django_dicom:
+           https://github.com/TheLabbingProject/django_dicom
+
+        Parameters
+        ----------
+        path : Path
+            Path to import *.dcm* files from
+        progressbar : bool, optional
+            Whether to print a progressbar or not, by default True
+        report : bool, optional
+            Whether to print a summary report, by default True
+
+        Returns
+        -------
+        QuerySet
+            The created DICOM
+            :class:`~django_dicom.models.series.Series` instances
+        """
+
         images = DicomImage.objects.import_path(
             path, progressbar=progressbar, report=report
         )
@@ -18,23 +48,57 @@ class ScanManager(Manager):
 
     def import_path(
         self, path: Path, progressbar: bool = True, report: bool = True
-    ) -> tuple:
+    ) -> dict:
+        """
+        Import MRI data from the given path.
+
+        Parameters
+        ----------
+        path : Path
+            Path to import *.dcm* files from
+        progressbar : bool, optional
+            Whether to print a progressbar or not, by default True
+        report : bool, optional
+            Whether to print a summary report, by default True
+
+        Returns
+        -------
+        dict
+            Created database instances by MRI scan type
+        """
+
         dicom_scans = self.import_dicom_data(path, progressbar, report)
         return {ScanType.DICOM.value: dicom_scans}
 
-    def scan_by_method(self, method: str) -> list:
-        mri_method_definitions = SequenceType.objects.get(
-            title__icontains=method
-        ).sequence_definitions
-        sequence_variants = [
-            definition["sequence_variant"]
-            for definition in mri_method_definitions
-        ]
-        scanning_sequences = [
-            definition["scanning_sequence"]
-            for definition in mri_method_definitions
-        ]
-        return self.filter(
-            dicom__sequence_variant__contains=list(sequence_variants),
-            dicom__scanning_sequence__contains=list(scanning_sequences),
-        )
+    def filter_by_sequence_type(self, title: str) -> QuerySet:
+        """
+        Returns scans with a SequenceType title matching the provided
+        *title* argument.
+
+        Parameters
+        ----------
+        title : str
+            SequenceType title
+
+        Returns
+        -------
+        QuerySet
+            Matching scans
+        """
+
+        sequence_type = SequenceType.objects.get(title__icontains=title)
+        if sequence_type:
+            definitions = sequence_type.sequence_definition_set.values(
+                "scanning_sequence", "sequence_variant"
+            )
+            definitions = [
+                {
+                    f"dicom__{key}": value
+                    for key, value in sequence_definition.items()
+                }
+                for sequence_definition in list(definitions)
+            ]
+            q_objects = Q()
+            for definition in definitions:
+                q_objects |= Q(**definition)
+            return self.filter(q_objects)
