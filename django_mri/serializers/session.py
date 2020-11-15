3504c8d5c58b71e86210e9de8f95ada1a4948e5e
Zvi Baratz <z.baratz@gmail.com>
2020-11-15 11:29:07 +0200

    Added a patch method to the SessionSerializer to allow partial updates and editing session comments through the FE.

--- django_mri/serializers/session.py
+++ django_mri/serializers/session.py
@@ -4,6 +4,7 @@ Definition of the :class:`SessionSerializer` class.
 from django_mri.models.session import Session
 from django_mri.serializers.utils import MiniSubjectSerializer
 from rest_framework import serializers
+from rest_framework.response import Response
 
 
 class SessionSerializer(serializers.HyperlinkedModelSerializer):
@@ -20,3 +21,29 @@ class SessionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Session
         fields = "id", "subject", "comments", "time"
+
+    def patch(self, request, pk) -> Response:
+        """
+        Handles "PATCH" requests.
+
+        Parameters
+        ----------
+        request : Request
+            HTTP request object
+        pk : int
+            Session primary key
+
+        Returns
+        -------
+        Response
+            HTTP response with updated instance or error message
+        """
+
+        instance = Session.objects.get(id=pk)
+        serializer = SessionSerializer(
+            instance=instance, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(code=201, data=serializer.data)
+        return Response(code=400, data=f"Failed to patch session #{pk}")
