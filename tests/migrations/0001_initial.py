790d1c40e3aa5ca0b837e9444df5540422a2c94a
Zvi Baratz <z.baratz@gmail.com>
2021-06-02 17:45:16 +0300

    Added laboratory model to test migration.

--- tests/migrations/0001_initial.py
+++ tests/migrations/0001_initial.py
@@ -131,4 +131,28 @@ class Migration(migrations.Migration):
                 ),
             ],
         ),
+        migrations.CreateModel(
+            name="Laboratory",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "title",
+                    models.CharField(max_length=255, verbose_name="title"),
+                ),
+                (
+                    "description",
+                    models.TextField(
+                        blank=True, null=True, verbose_name="description"
+                    ),
+                ),
+            ],
+        ),
     ]
