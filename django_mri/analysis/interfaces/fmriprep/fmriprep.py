8f5b7f9d39cffeb69295efa0e3df8e8d39359a11
Gal Hershkovitz <hershkovitz1@mail.tau.ac.il>
2021-06-07 17:59:12 +0300

    fmriprep interface keeps building up

--- django_mri/analysis/interfaces/fmriprep/fmriprep.py
+++ django_mri/analysis/interfaces/fmriprep/fmriprep.py
@@ -53,79 +53,138 @@ class fMRIprep:
     # Outputs
     DEFAULT_OUTPUTS = {
         ## Anatomicals ##
-        "native_T1w": ["anat", "desc-preproc_T1w.nii.gz"],
-        "native_brain_mask": ["anat", "desc-brain_mask.nii.gz"],
-        "native_parcellation": ["anat", "dseg.nii.gz"],
-        "native_csf": ["anat", "CSF_probseg.nii.gz"],
-        "native_gm": ["anat", "GM_probseg.nii.gz"],
-        "native_wm": ["anat", "WM_probseg.nii.gz"],
+        "native_T1w": ["fmriprep", "anat", "desc-preproc_T1w.nii.gz"],
+        "native_brain_mask": ["fmriprep", "anat", "desc-brain_mask.nii.gz"],
+        "native_parcellation": ["fmriprep", "anat", "dseg.nii.gz"],
+        "native_csf": ["fmriprep", "anat", "CSF_probseg.nii.gz"],
+        "native_gm": ["fmriprep", "anat", "GM_probseg.nii.gz"],
+        "native_wm": ["fmriprep", "anat", "WM_probseg.nii.gz"],
         "standard_T1w": [
+            "fmriprep",
             "anat",
             "space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz",
         ],
         "standard_brain_mask": [
+            "fmriprep",
             "anat",
             "space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz",
         ],
-        "standard_parcellation": ["anat", "space-MNI152NLin*dseg.nii.gz"],
+        "standard_parcellation": [
+            "fmriprep",
+            "anat",
+            "space-MNI152NLin*dseg.nii.gz",
+        ],
         "standard_csf": [
+            "fmriprep",
             "anat",
             "space-MNI152NLin2009cAsym_label-CSF_probseg.nii.gz",
         ],
         "standard_gm": [
+            "fmriprep",
             "anat",
             "space-MNI152NLin2009cAsym_label-GM_probseg.nii.gz",
         ],
         "standard_wm": [
+            "fmriprep",
             "anat",
             "space-MNI152NLin2009cAsym_label-WM_probseg.nii.gz",
         ],
         "native_to_mni_transform": [
+            "fmriprep",
             "anat",
             "from-T1w_to-MNI152NLin2009cAsym__mode-image_xfm.h5",
         ],
         "mni_to_native_transform": [
+            "fmriprep",
             "anat",
             "from-MNI152NLin2009cAsym_to-T1w_mode-image_xfm.h5",
         ],
         "native_to_fsnative_transform": [
+            "fmriprep",
             "anat",
             "from-T1w_to-fsnative_mode-image_xfm.h5",
         ],
         "fsnative_to_native_transform": [
+            "fmriprep",
             "anat",
             "from-fsnative_to-T1w_mode-image_xfm.h5",
         ],
-        "smoothwm": ["anat", "smoowthwm.surf.gii"],
-        "pial": ["anat", "pial.surf.gii"],
-        "midthickness": ["anat", "midthickness.surf.gii"],
-        "inflated": ["anat", "inflated.surf.gii"],
+        "smoothwm": ["fmriprep", "anat", "smoowthwm.surf.gii"],
+        "pial": ["fmriprep", "anat", "pial.surf.gii"],
+        "midthickness": ["fmriprep", "anat", "midthickness.surf.gii"],
+        "inflated": ["fmriprep", "anat", "inflated.surf.gii"],
         ## Functionals ##
-        "native_boldref": ["func", "space-T1w_desc-boldref.nii.gz"],
-        "native_func_brain_mask": ["func", "space-T1w_desc-brain_mask.nii.gz"],
-        "native_preproc_bold": ["func", "space-T1w_desc-preproc_bold.nii.gz"],
-        "native_aparc_bold": ["func", "space-T1w_desc-aparcaseg_dseg.nii.gz"],
-        "native_aseg_bold": ["func", "space-T1w_desc-aseg_dseg.nii.gz"],
+        "native_boldref": [
+            "fmriprep",
+            "func",
+            "space-T1w_desc-boldref.nii.gz",
+        ],
+        "native_func_brain_mask": [
+            "fmriprep",
+            "func",
+            "space-T1w_desc-brain_mask.nii.gz",
+        ],
+        "native_preproc_bold": [
+            "fmriprep",
+            "func",
+            "space-T1w_desc-preproc_bold.nii.gz",
+        ],
+        "native_aparc_bold": [
+            "fmriprep",
+            "func",
+            "space-T1w_desc-aparcaseg_dseg.nii.gz",
+        ],
+        "native_aseg_bold": [
+            "fmriprep",
+            "func",
+            "space-T1w_desc-aseg_dseg.nii.gz",
+        ],
         "standard_boldref": [
+            "fmriprep",
             "func",
             "space-MNI152NLin2009cAsym_desc-boldref.nii.gz",
         ],
         "standard_func_brain_mask": [
+            "fmriprep",
             "func",
             "space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz",
         ],
         "standard_preproc_bold": [
+            "fmriprep",
             "func",
             "space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz",
         ],
         "standard_aparc_bold": [
+            "fmriprep",
             "func",
             "space-MNI152NLin2009cAsym_desc-aparcaseg_dseg.nii.gz",
         ],
         "standard_aseg_bold": [
+            "fmriprep",
             "func",
             "space-MNI152NLin2009cAsym_desc-aseg_dseg.nii.gz",
         ],
+        "confounds_tsv": ["fmriprep", "func", "desc-confound_timeseries.tsv"],
+        "confounds_json": [
+            "fmriprep",
+            "func",
+            "desc-confound_timeseries.json",
+        ],
+        "freesurfer_T1": ["freesurfer", "mri", "T1.mgz"],
+        "freesurfer_rawavg": ["freesurfer", "mri", "rawavg.mgz"],
+        "freesurfer_orig": ["freesurfer", "mri", "orig.mgz"],
+        "freesurfer_nu": ["freesurfer", "mri", "nu.mgz"],
+        "freesurfer_norm": ["freesurfer", "mri", "norm.mgz"],
+        "freesurfer_aseg": ["freesurfer", "mri", "aseg.mgz"],
+        "freesurfer_aseg_stats": ["freesurfer", "stats", "aseg.stats"],
+        "freesurfer_brain": ["freesurfer", "mri", "brain.mgz"],
+        "freesurfer_brainmask": ["freesurfer", "mri", "brainmask.mgz"],
+        "freesurfer_filled": ["freesurfer", "mri", "filled.mgz"],
+        "freesurfer_wm": ["freesurfer", "mri", "wm.mgz"],
+        "freesurfer_wmparc": ["freesurfer", "mri", "wmparc.mgz"],
+        "freesurfer_wmparc_stats": ["freesurfer", "stats", "wmparc.stats"],
+        "freesurfer_BA_stats": ["freesurfer", "stats", "*.BA_exvivo*.stats"],
+        ### NEEDS COMPLETION ###
     }
 
     __version__ = "BETA"
@@ -168,7 +227,9 @@ class fMRIprep:
         command = f"singularity run --cleanenv -B {bids_dir.parent}:/work -B {destination.parent}:/output /my_images/fmriprep-latest.simg /work/{bids_dir.name} /output/{destination.name} {analysis_level}"
         return command + self.set_configuration_by_keys()
 
-    def find_output(self, destination: Path, partial_output: str):
+    def find_output(
+        self, destination: Path, partial_output: str, subj_id: str
+    ):
         """
         uses the destination and some default dictionary to locate specific output files of *fmriprep*
         Parameters
@@ -178,7 +239,7 @@ class fMRIprep:
         partial_output : str
             A string that identifies a specific output
         """
-        sub_dir, output_id = partial_output.split("/")
+        sub_dir, output_id = self.DEFAULT_OUTPUTS.get(partial_output)
         output = [f for f in destination.rglob(f"{sub_dir}/*{output_id}")]
 
     def generate_output_dict(self, destination: Path) -> dict:
