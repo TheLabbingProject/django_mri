5eae3ce0a8cf5f692b057f5ab3856b66d1e01afa
Zvi Baratz <z.baratz@gmail.com>
2021-09-13 16:17:58 +0300

    Added some utility methods to the Session model for easier file export.

--- django_mri/models/session.py
+++ django_mri/models/session.py
@@ -1,6 +1,9 @@
 """
 Definition of the :class:`Session` class.
 """
+from pathlib import Path
+from typing import List
+
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 from django_mri.models import help_text
@@ -16,6 +19,12 @@ Group = get_group_model()
 MeasurementDefinition = get_measurement_model()
 Study = get_study_model()
 Subject = get_subject_model()
+
+SECONDS_IN_YEAR: int = 60 * 60 * 24 * 365
+CLAIMED_SESSION_STRING: str = "Subject #{subject_id} MRI session from {date}"
+UNCLAIMED_SESSION_STRING: str = "Unclaimed MRI session from {date}"
+DICOM_FILES_KEY: str = "dicom__image__dcm"
+NIFTI_FILES_KEY: str = "_nifti__path"
 
 
 class Session(TimeStampedModel):
@@ -77,8 +86,52 @@ class Session(TimeStampedModel):
         """
         date = self.time.date()
         if self.subject:
-            return f"Subject #{self.subject.id} MRI session from {date}"
-        return f"Unclaimed MRI session from {date}"
+            return CLAIMED_SESSION_STRING.format(
+                subject_id=self.subject.id, date=date
+            )
+        return UNCLAIMED_SESSION_STRING.format(date=date)
+
+    def infer_subject_age(self) -> float:
+        """
+        Returns the subject's age in years at the time of the session
+        acquisition. If the subject's date of birth or the session's
+        acquisition time are not available, returns `None`.
+
+        See Also
+        --------
+        * :func:`subject_age`
+
+        Returns
+        -------
+        float
+            Subject age in years at the time of the session's acquisition
+        """
+        has_required_info = (
+            self.time and self.subject and self.subject.date_of_birth
+        )
+        if has_required_info:
+            delta = self.time.date() - self.subject.date_of_birth
+            return delta.total_seconds() / SECONDS_IN_YEAR
+
+    def list_dicom_files(self) -> List[Path]:
+        return [
+            Path(p)
+            for p in self.scan_set.values_list(DICOM_FILES_KEY, flat=True)
+        ]
+
+    def list_nifti_files(self, include_json: bool = True) -> List[Path]:
+        associated_niis = self.scan_set.filter(
+            _nifti__isnull=False
+        ).values_list(NIFTI_FILES_KEY, flat=True)
+        paths = []
+        for path in associated_niis:
+            p = Path(path)
+            paths.append(p)
+            if include_json:
+                json_path = (p.parent / p.stem).with_suffix(".json")
+                if json_path.exists():
+                    paths.append(json_path)
+        return paths
 
     @property
     def subject_age(self) -> float:
@@ -87,19 +140,28 @@ class Session(TimeStampedModel):
         acquisition. If the subject's date of birth or the session's
         acquisition time are not available, returns `None`.
 
+        See Also
+        --------
+        * :func:`infer_subject_age`
+
         Returns
         -------
         float
             Subject age in years at the time of the session's acquisition
         """
-
-        conditions = self.time and self.subject and self.subject.date_of_birth
-        if conditions:
-            delta = self.time.date() - self.subject.date_of_birth
-            return delta.total_seconds() / (60 * 60 * 24 * 365)
+        return self.infer_subject_age()
 
     @property
     def study_groups(self) -> models.QuerySet:
+        """
+        Returns a queryset of associated :class:`~research.models.group.Group`
+        instances.
+
+        Returns
+        -------
+        models.QuerySet
+            Associated study groups
+        """
         return Group.objects.filter(
             id__in=self.scan_set.values("study_groups")
         )
