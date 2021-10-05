a9d880e73efc4966892a833fa258f88d25fafbe6
Zvi Baratz <z.baratz@gmail.com>
2021-10-05 12:00:08 +0300

    Fixed associated studies filter.

--- django_mri/filters/session_filter.py
+++ django_mri/filters/session_filter.py
@@ -1,6 +1,9 @@
 """
 Definition of the :class:`SessionFilter` class.
 """
+from typing import List
+
+from django.db.models import Q, QuerySet
 from django_filters import rest_framework as filters
 from django_mri.filters.scan_filter import NumberInFilter
 from django_mri.filters.utils import LOOKUP_CHOICES
@@ -21,9 +24,9 @@ class SessionFilter(filters.FilterSet):
         field_name="id", lookup_expr="in", label="Session ID is in"
     )
     study_id_in = NumberInFilter(
-        field_name="scan__study_groups__study__id",
         lookup_expr="in",
-        label="Study ID is in",
+        label="Associated Study ID is in",
+        method="filter_by_study_association",
     )
     subject_id_in = NumberInFilter(
         field_name="subject__id", lookup_expr="in", label="Subject ID is in"
@@ -51,3 +54,12 @@ class SessionFilter(filters.FilterSet):
             "scan_set",
             "session_date",
         )
+
+    def filter_by_study_association(
+        self, queryset: QuerySet, name: str, value: List[str]
+    ):
+        query = Q(scan__study_groups__study__id__in=value) | Q(
+            measurement__procedure__study__id__in=value
+        )
+        print(f"Filtering by studies: {value}")
+        return queryset.filter(query).distinct()
