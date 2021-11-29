0190740d48ce3d2e7e47f7e7a6ddeb95931a9fa6
Zvi Baratz <z.baratz@gmail.com>
2021-11-29 16:00:51 +0200

    Improvements to NIfTI conversion functionality.

--- django_mri/models/managers/session.py
+++ django_mri/models/managers/session.py
@@ -1,12 +1,15 @@
 """
 Definition of the :class:`SessionQuerySet` class.
 """
+import logging
+
 import pandas as pd
 from bokeh.plotting import Figure
 from django.db.models import Count
 from django.db.models.query import QuerySet
-
+from django_mri.models.managers import logs
 from django_mri.plots.session import plot_measurement_by_month
+from tqdm import tqdm
 
 #: Session fields to include in an exported DataFrame.
 DATAFRAME_FIELDS = (
@@ -40,6 +43,8 @@ class SessionQuerySet(QuerySet):
     .. _Django's Manager documentation:
        https://docs.djangoproject.com/en/3.1/topics/db/managers/#creating-a-manager-with-queryset-methods
     """
+
+    _logger = logging.getLogger("data.mri.session")
 
     def get_scan_set(self) -> QuerySet:
         """
@@ -79,3 +84,59 @@ class SessionQuerySet(QuerySet):
         df = pd.DataFrame(values)
         df.columns = DATAFRAME_COLUMNS
         return df.set_index("ID").sort_index()
+
+    def convert_to_nifti(
+        self,
+        force: bool = False,
+        persistent: bool = True,
+        progressbar: bool = True,
+        progressbar_position: int = 0,
+    ):
+        # Log and return if the queryset is empty.
+        if not self.exists():
+            abort_log = logs.SESSION_SET_NIFTI_CONVERSION_EMPTY.format()
+            self._logger.debug(abort_log)
+            return
+        # Run chronologically in reverse.
+        queryset = self.order_by("-time")
+        # Log session queryset conversion start.
+        start_log = logs.SESSION_SET_NIFTI_CONVERSION_START.format(
+            count=queryset.count()
+        )
+        self._logger.debug(start_log)
+        # Create progressbar if required.
+        iterator = (
+            tqdm(
+                queryset,
+                desc="Sessions",
+                unit="session",
+                position=progressbar_position,
+                leave=not progressbar_position,
+            )
+            if progressbar
+            else queryset
+        )
+        n_converted = 0
+        # Convert sessions to NIfTI.
+        try:
+            for session in iterator:
+                session.convert_to_nifti(
+                    force=force,
+                    persistent=persistent,
+                    progressbar=progressbar,
+                    progressbar_position=progressbar_position + 1,
+                )
+                n_converted += 1
+        except Exception as e:
+            # Log exception and re-raise.
+            failure_log = logs.SESSION_SET_NIFTI_CONVERSION_FAILURE.format(
+                n_converted=n_converted, n_total=queryset.count(), exception=e
+            )
+            self._logger.warn(failure_log)
+            raise
+        else:
+            # Log conversion success.
+            success_log = logs.SESSION_SET_NIFTI_CONVERSION_SUCCESS.format(
+                count=queryset.count()
+            )
+            self._logger.debug(success_log)
