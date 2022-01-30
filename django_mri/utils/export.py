382044898965985fbc35a42db4e3991345d09132
Zvi Baratz <z.baratz@gmail.com>
2022-01-30 15:34:25 +0200

    Added export mutator for fmriprep and dmriprep.

--- django_mri/utils/export.py
+++ django_mri/utils/export.py
@@ -3,11 +3,21 @@ from pathlib import Path
 from django.conf import settings
 from django_mri.utils import get_bids_dir, get_mri_root
 
+MRI_ROOT = get_mri_root()
+
+
+def get_media_root() -> Path:
+    return Path(settings.MEDIA_ROOT)
+
+
+def get_bids_root() -> Path:
+    MEDIA_ROOT = get_media_root()
+    return MEDIA_ROOT / get_bids_dir()
+
 
 def get_recon_all_export_destination(run) -> Path:
-    MRI_ROOT = get_mri_root()
-    MEDIA_ROOT = Path(settings.MEDIA_ROOT)
-    BIDS_DIR = MEDIA_ROOT / get_bids_dir()
+    MEDIA_ROOT = get_media_root()
+    BIDS_ROOT = get_bids_root()
     t1_files = [
         i for i in run.input_set.all() if i.definition.key == "T1_files"
     ][0]
@@ -16,9 +26,24 @@ def get_recon_all_export_destination(run) -> Path:
         str(run.analysis_version).replace(" ", "_").replace(".", "").lower()
     )
     raw_destination = (
-        MRI_ROOT / "derivatives" / analysis_id / path.relative_to(BIDS_DIR)
+        MRI_ROOT / "derivatives" / analysis_id / path.relative_to(BIDS_ROOT)
     ).relative_to(MEDIA_ROOT)
     return Path(str(raw_destination).split(".")[0])
 
 
-EXPORT_MUTATORS = {"ReconAll": get_recon_all_export_destination}
+def get_fmriprep_export_destination(run) -> Path:
+    MEDIA_ROOT = get_media_root()
+    analysis_id = (
+        str(run.analysis_version).replace(" ", "_").replace(".", "").lower()
+    )
+    participant_label = run.get_input("participant_label")[0]
+    return (
+        MRI_ROOT / "derivatives" / analysis_id / f"sub-{participant_label}"
+    ).relative_to(MEDIA_ROOT)
+
+
+EXPORT_MUTATORS = {
+    "ReconAll": get_recon_all_export_destination,
+    "fMRIPrep": get_fmriprep_export_destination,
+    "dMRIPrep": get_fmriprep_export_destination,
+}
