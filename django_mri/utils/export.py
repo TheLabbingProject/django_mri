7b9a8832b7191ceedafd1af123f5213b52e4df15
Zvi Baratz <z.baratz@gmail.com>
2022-01-30 14:55:22 +0200

    Added experimental export mutator for recon all results.

---
+++ django_mri/utils/export.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from django_mri.utils import get_bids_dir, get_mri_root
+
+BIDS_DIR = get_bids_dir()
+MRI_ROOT = get_mri_root()
+
+
+def get_recon_all_export_destination(run) -> Path:
+    t1_files = [
+        i for i in run.input_set.all() if i.definition.key == "T1_files"
+    ][0]
+    path = Path(t1_files.query_related_instance()[0].path)
+    raw_destination = MRI_ROOT / "derivatives" / path.relative_to(BIDS_DIR)
+    return Path(str(raw_destination).split(".")[0])
+
+
+EXPORT_MUTATORS = {"ReconAll": get_recon_all_export_destination}
