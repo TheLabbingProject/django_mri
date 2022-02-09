bc4d3eee0bbc67b564041dc5fabc0c2abc979090
Zvi Baratz <z.baratz@gmail.com>
2022-02-09 10:51:48 +0200

    Added today_only option to faciliate Siemens style scheduled archive imports.

--- django_mri/tasks.py
+++ django_mri/tasks.py
@@ -1,3 +1,7 @@
+"""
+Celery tasks provided by the *django_mri* app.
+"""
+from datetime import datetime
 from pathlib import Path
 from typing import Union
 
@@ -8,7 +12,9 @@ from django_mri.models.scan import Scan
 
 
 @shared_task(name="django_mri.import-data")
-def import_data(data_directory: Union[int, str, Path]) -> list:
+def import_data(
+    data_directory: Union[int, str, Path], today_only: bool = False
+) -> list:
     """
     Imports new data (unfamiliar subdirectories) from the provided
     :class:`~django_mri.models.data_directory.DataDirectory` instance.
@@ -17,6 +23,9 @@ def import_data(data_directory: Union[int, str, Path]) -> list:
     ----------
     data_directory : Union[int, str, Path]
         Directory path or DataDirectory instance ID
+    today_only : bool
+        Whether to look for a '<year>/<month>/<day>' subdirectory to import,
+        default is False
 
     Returns
     -------
@@ -30,6 +39,14 @@ def import_data(data_directory: Union[int, str, Path]) -> list:
         imported_subdirectories = source.sync(progressbar=False, report=True)
         return imported_subdirectories
     else:
+        if today_only:
+            today = datetime.today()
+            data_directory = (
+                Path(data_directory)
+                / str(today.year)
+                / str(today.month)
+                / str(today.day)
+            )
         Scan.objects.import_path(
             data_directory, progressbar=False, report=False
         )
